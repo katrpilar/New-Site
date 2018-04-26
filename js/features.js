97079ce008e108b451a41d4e5d5022fd87ecd18c
Katrina Brinson <kbrinso@gmail.com>
2018-04-25 18:35:03 -0700

    Initial commit

---
+++ js/features.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('.parallax').parallax();
+  $('.slider').slider();
+  $('.modal').modal();
+  $('.carousel.featured-products').carousel({
+  	padding: 50,
+  	shift: 30
+  	});
+});
